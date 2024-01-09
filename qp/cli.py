ce0c8439bf92e8a19545dd449d1dbb7c440a17f3
David Kastner <kastner@mit.edu>
2024-01-09 15:31:39 -0500

    Update cli.py

--- qp/cli.py
+++ qp/cli.py
@@ -7,10 +7,10 @@ Options:
 ==================== =====
 -i                   Input PDB code, PDB file, or batch file [required]
 -o                   Output directory  [required]
--m, -\-modeller      Use MODELLER to add missing loops 
--p, -\-protoss       Use Protoss to add hydrogens
--c, -\-coordination  Select the first, second, etc. coordination spheres
--s, -\-skip          Skips rerunning MODELLER or Protoss if an
+-m, --modeller       Use MODELLER to add missing loops 
+-p, --protoss        Use Protoss to add hydrogens
+-c, --coordination   Select the first, second, etc. coordination spheres
+-s, --skip           Skips rerunning MODELLER or Protoss if an
                      existing output file is found
 ==================== =====
 
